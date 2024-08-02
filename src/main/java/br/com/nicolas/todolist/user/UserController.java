21ae066e04c6de5cc724e4487fbca45bbfeee09a
Nicolas Zalasik <nizalasik@gmail.com>
2024-08-02 10:30:28 -0300

    Configurado bd

--- src/main/java/br/com/nicolas/todolist/user/UserController.java
+++ src/main/java/br/com/nicolas/todolist/user/UserController.java
@@ -1,9 +1,9 @@
 package br.com.nicolas.todolist.user;
 
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.PostMapping;
 
 
 @RestController
@@ -12,8 +12,6 @@ public class UserController {
     
     @PostMapping("/")
     public void create(@RequestBody UserModel userModel) {
-        System.out.println(userModel.name);
-        System.out.println(userModel.username);
-        System.out.println(userModel.password);
+        System.out.println(userModel.getUsername());
     }
 }
--- src/main/java/br/com/nicolas/todolist/user/UserModel.java
+++ src/main/java/br/com/nicolas/todolist/user/UserModel.java
@@ -1,8 +1,27 @@
 package br.com.nicolas.todolist.user;
 
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+import org.hibernate.annotations.CreationTimestamp;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+@Data
+@Entity(name = "t_usuario")
 public class UserModel {
     
+    @Id
+    @GeneratedValue(generator = "UUID")
+    private UUID id;
+
     public String username;
     public String name;
     public String password;
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
 }
