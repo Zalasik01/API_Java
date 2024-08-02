912b0af6e713735bd3b4170822a8a52955a40e61
Nicolas Zalasik <nizalasik@gmail.com>
2024-08-02 14:11:57 -0300

    Captando usuario e senha e separando eles

---
+++ src/main/java/br/com/nicolas/todolist/filter/FilterTaskAuth.java
@@ -0,0 +1,49 @@
+package br.com.nicolas.todolist.filter;
+
+import java.io.IOException;
+import java.util.Base64;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@Component
+public class FilterTaskAuth extends OncePerRequestFilter{
+
+
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+            throws ServletException, IOException {
+        
+                // Pegar Usuário e Senha
+                var authorization = request.getHeader("Authorization");
+                
+                var authEncoded = authorization.substring("Basic".length()).trim();
+
+                byte[] authDecode = Base64.getDecoder().decode(authEncoded);
+
+                var authString = new String(authDecode);
+                
+
+                String[] credentials = authString.split(":");
+                String username = credentials[0];
+                String password = credentials[1];
+                System.out.println("Authorization");
+                System.out.println(username);
+                System.out.println(password);
+
+                // Validar usuário
+
+
+                // Validar senha
+
+                filterChain.doFilter(request, response);
+            
+            
+    }    
+}
